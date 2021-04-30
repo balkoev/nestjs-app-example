187a696d1490123e90020c4e4be5fb4960f0fe4a
Adam Balkoev <balkoev@MacBook-Pro.local>
2021-04-30 11:55:21 +0300

    fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,7 +14,6 @@ import { Roles } from 'src/auth/roles-auth.decorator';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { AddRoleDto } from './dto/add-role.dto';
 import { BanUserDto } from './dto/ban-user.dto';
-import { ValidationPipe } from 'src/pipes/validation.pipe';
 
 @ApiTags('Пользователи')
 @Controller('users')
@@ -23,7 +22,6 @@ export class UsersController {
 
   @ApiOperation({ summary: 'Создание пользователя' })
   @ApiResponse({ status: 200, type: User })
-  // @UsePipes(ValidationPipe)
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
