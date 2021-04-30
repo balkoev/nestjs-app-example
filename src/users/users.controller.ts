db004bc7f42dec1eeb19cd863b6af7548586ba44
Adam Balkoev <balkoev@MacBook-Pro.local>
2021-04-30 12:46:30 +0300

    Добавил статику

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  UseGuards,
-  UsePipes,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
